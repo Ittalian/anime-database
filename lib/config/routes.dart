3152163b4f5c6cf969bcf1eb55ade1801a682f0d
Ittalian <bothmather@au.com>
2024-09-26 11:39:42 +0900

    ルート作成

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -1,11 +1,14 @@
 import 'package:anime_database/models/anime.dart';
 import 'package:anime_database/models/review.dart';
+import 'package:anime_database/utils/models/search_anime.dart';
 import 'package:anime_database/views/pages/my_anime/my_anime.dart';
 import 'package:anime_database/views/pages/my_anime/my_anime_index.dart';
+import 'package:anime_database/views/pages/search/search_result.dart';
 import 'package:flutter/material.dart';
 
 class Routes {
-  static const String search = 'search';
+  static const String searchResult = 'search_result';
+  static const String searchDetails = 'search_details';
   static const String myAnimeIndex = 'my_anime_index';
   static const String myAnimeAdd = 'my_anime_add';
   static const String myAnimeEdit = 'my_anime_edit';
@@ -23,6 +26,9 @@ class Routes {
                   anime: myAnimeOptions['anime'] as Anime,
                   review: myAnimeOptions['review'] as Review,
                 ));
+      case searchResult:
+        final searchResultOptions = settings.arguments as Map;
+        return MaterialPageRoute(builder: (_) => SearchResult(animes: searchResultOptions['animes'] as List<SearchAnime>));
       default:
         return MaterialPageRoute(
             builder: (_) => Scaffold(
