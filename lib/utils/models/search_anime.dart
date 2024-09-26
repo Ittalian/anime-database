b348991f97e0bf8577a7fe5537729a54ffef4a7f
Ittalian <bothmather@au.com>
2024-09-26 11:39:34 +0900

    レスポンスを格納するクラス作成

---
+++ lib/utils/models/search_anime.dart
@@ -0,0 +1,11 @@
+class SearchAnime {
+  String animeTitle;
+  String publicUrl;
+  String xAccountUrl;
+
+  SearchAnime({
+    required this.animeTitle,
+    required this.publicUrl,
+    required this.xAccountUrl,
+  });
+}
